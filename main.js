791396438b87470c346e5f2ae6ed2a4bf577488b
Mateo Trucco <mateotrucco.arg@gmail.com>
2023-04-24 20:10:18 -0300

    Commit casi inicial

--- main.js
+++
@@ -1,7 +0,0 @@
-const buttons = document.querySelectorAll(".oBut");
-
-buttons.forEach(button => {
-  button.addEventListener("click", function() {
-    let val = this.classList.toggle("on");
-  });
-});
